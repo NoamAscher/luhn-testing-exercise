b32c1b3b18886b97a19e03eab7905b8a09e82eba
Noam Ascher <noam.ascher@gmail.com>
2016-10-08 02:49:56 +0000

    Completed 8 tests, 4 true and 4 false, 4 string-input and 4 number-input.

--- test/test_luhn.js
+++ test/test_luhn.js
@@ -1,28 +1,64 @@
-/* palindrome tests - to be changed to luhn tests
-
 var assert = require("chai").assert;
-var isPalindrome = require("../lib/palindromes");
+var luhnCheck = require("../lib/luhn");
 
-describe("Palindromes", function() {
-  it("should return true if a word is a palindrome", function() {
-    var word = "abba";
-    var result = isPalindrome(word);
+// luhnCheck("79927398713")
+// luhnCheck(79927398716)
+// luhnCheck("79927398713")
+// luhnCheck(79927398716)
+
+// From https://www.rosettacode.org/wiki/Luhn_test_of_credit_card_numbers
+// 49927398716: T
+// 49927398717: F
+// 1234567812345678: F
+// 1234567812345670: T
+
+describe("Luhn", function() {
+  it("should return true for the string \"49927398716\"", function() {
+    var num = "49927398716";
+    var result = luhnCheck(num);
     assert.isTrue(result);
   });
 
-  it("should return false if a word is NOT a palindrome", function() {
-    var word = "not";
-    assert.isFalse(isPalindrome(word));
+  it("should return true for the number 49927398716", function() {
+    var num = 49927398716;
+    var result = luhnCheck(num);
+    assert.isTrue(result);
   });
 
-  it("should return true if a phrase is a palindrome", function() {
-    var phrase = "a man a plan a canal panama";
-    assert.isTrue(isPalindrome(phrase));
+  it("should return false for the string \"49927398717\"", function() {
+    var num = "49927398717";
+    var result = luhnCheck(num);
+    assert.isFalse(result);
   });
 
-  it("should return false if a phrase is NOT a palindrome", function() {
-    var phrase = "this is not a palindrome"
-    assert.isFalse(isPalindrome(phrase));
+  it("should return false for the number 49927398717", function() {
+    var num = 49927398717;
+    var result = luhnCheck(num);
+    assert.isFalse(result);
+  });
+
+  it("should return false for the string \"1234567812345678\"", function() {
+    var num = "1234567812345678";
+    var result = luhnCheck(num);
+    assert.isFalse(result);
+  });
+
+  it("should return false for the number 1234567812345678", function() {
+    var num = 1234567812345678;
+    var result = luhnCheck(num);
+    assert.isFalse(result);
+  });
+
+  it("should return true for the string \"1234567812345670\"", function() {
+    var num = "1234567812345670";
+    var result = luhnCheck(num);
+    assert.isTrue(result);
+  });
+
+  it("should return true for the number 1234567812345670", function() {
+    var num = 1234567812345670;
+    var result = luhnCheck(num);
+    assert.isTrue(result);
   });
 });
 
